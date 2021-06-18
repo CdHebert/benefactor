98c25652e67b9a5046cbb20660ad6b355ec1648a
Daniel <dkbaffour777@gmail.com>
2021-06-17 19:34:31 -0700

    seed models

--- models/index.js
+++ models/index.js
@@ -2,25 +2,45 @@
 const Product = require('./Product');
 const List = require('./List');
 const User = require('./User');
+const Friend = require('./Friend');
+const UserFriend = require('./UserFriend');
 
-// Products belongsTo List
-Product.belongsTo(List, {
-  foreignKey: 'list_id',
+/* USER AND FRIEND ASSOCIATION */
+// Friends belongsTo User
+User.belongsToMany(Friend, {
+  through: UserFriend,
+  foreignKey: 'u_id',
 });
+// User has many Friends
+Friend.belongsToMany(User, {
+  through: UserFriend,
+  foreignKey: 'f_id',
+});
+
+/* USER AND LIST ASSOCIATION */
+// Lists belongTo User (through UserList)
+User.hasMany(List, {
+  foreignKey: 'user_id'
+});
+// User belongsToMany Lists (through UserList)
+List.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+/* LIST AND PRODUCT ASSOCIATION */
 // List has many Products
 List.hasMany(Product, {
   foreignKey: 'list_id',
 });
-// Lists belongs to User
-List.belongsTo(User, {
-  foreignKey: 'user_id'
+// Products belongsTo List
+Product.belongsTo(List, {
+  foreignKey: 'list_id',
 });
-// User has many Lists
-User.hasMany(List, {
-  foreignKey: 'user_id'
-});
+
 module.exports = {
   Product,
   List,
   User,
+  Friend,
+  UserFriend,
 };

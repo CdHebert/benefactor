0a1c8389e5330f5d7b153c2236e095d20af1334a
CBear <chebert222@gmail.com>
2021-06-17 21:16:00 -0700

    login front end done

--- models/Product.js
+++ models/Product.js
@@ -7,7 +7,7 @@ const sequelize = require('../config/connection');
 class Product extends Model {}
 
 // set up fields and rules for Product model
-Product.init(
+Product.init( 
   {
     id: {
       type: DataTypes.INTEGER,

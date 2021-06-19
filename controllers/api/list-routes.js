3c4c2e55792cd7ec6b759b40f807dd0ae3c45c6e
Christi-Lee <christileemc@gmail.com>
2021-06-18 22:46:54 -0700

    list-routes created  - get, get one, create, update, delete

--- controllers/api/list-routes.js
+++ controllers/api/list-routes.js
@@ -1,12 +1,61 @@
 const router = require('express').Router();
-const { List } = require('../../models');
+const { List, Product } = require('../../models');
 
 // The `/api/lists` endpoint
+
 
 router.get('/', (req, res) => {
   // find all lists
   // be sure to include its associated Products
-  List.findAll({}).then(lists => res.json(lists));
+  List.findAll({
+    include: [Product]
+  }).then(lists => res.json(lists)).catch(err => res.json(err))
 });
+
+router.get('/:id', (req, res) => {
+  // find one
+  // be sure to include its associated Products
+  List.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Product]
+  }).then(lists => res.json(lists)).catch(err => res.json(err))
+});
+
+router.post('/', (req, res) => {
+  // create
+    List.create(req.body).then(lists => res.json(lists)).catch(err => res.json(err))
+});
+
+// update list
+router.put('/:list:listId', (req, res, next) => {
+  List.update (
+    {list_name: req.body.list_name},
+    {returning: true, where: {id: req.params.listId} }
+  )
+  .then(function([ rowsUpdate, [updatedList] ]) {
+    res.json(updatedList)
+  })
+  .catch(next)
+ })
+
+  // delete list
+  const deleteList = async (req, res) => {
+    try {
+      const { listId } = req.params;
+      const deleted = await models.List.destroy({
+        where: { id: listId }
+      });
+      if (deleted) {
+        return res.status(204).send("List deleted");
+      }
+      throw new Error("List not found");
+    } catch (error) {
+      return res.status(500).send(error.message);
+    }
+  };
+
+
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 const apiRoutes = require('./api/');
 
 router.use('/api', apiRoutes);
-
+router.put('/api/list-routes/:updateList', controllers.updateList);
+router.delete('/api/list-routes:listId', controllers.deleteList);
 module.exports = router;

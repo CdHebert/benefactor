effb8164128ee71dd611059db06dacad2ffa9be4
CBear <chebert222@gmail.com>
2021-06-19 10:48:23 -0700

    main and homepage done

--- server.js
+++ server.js
@@ -30,10 +30,10 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening on port ' + PORT));
-});
+}); 

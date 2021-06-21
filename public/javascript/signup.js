3c1cd1b6dc9d689e974d1c6e1e9b53caee627c0b
CBear <chebert222@gmail.com>
2021-06-21 13:50:38 -0700

    added login functionality

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,11 +1,11 @@
-async function loginFormHandler(event) {
+  async function signupFormHandler(event) {
     event.preventDefault();
   
-    const username = document.querySelector('#username-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
   
     if (username && password) {
-      const response = await fetch('/api/users/login', {
+      const response = await fetch('/api/users', {
         method: 'post',
         body: JSON.stringify({
           username,
@@ -21,10 +21,5 @@ async function loginFormHandler(event) {
       }
     }
   }
-  
 
-  
-  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
-  
-  
-  
+  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);

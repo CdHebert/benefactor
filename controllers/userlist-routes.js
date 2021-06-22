1bf6aecb7c1ef9f2a7ed1bae9548b08e51b8916a
LifeInvader <matthewkausfox@hotmail.com>
2021-06-22 17:07:03 -0600

    fixed input bar

---
+++ controllers/userlist-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const withAuth = require('../utils/auth')
+const { User, List, Friend, UserFriend, Product } = require('../models');
+
+
+router.get('/', (req, res) => {
+    res.render('userboard')
+})
+
+
+
+module.exports = router;

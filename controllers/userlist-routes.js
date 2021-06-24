650272192f8fd8a3d7398ea7e5a89af1114713e7
CBear <chebert222@gmail.com>
2021-06-23 17:30:37 -0700

    added personal page

--- controllers/userlist-routes.js
+++ controllers/userlist-routes.js
@@ -4,10 +4,19 @@ const withAuth = require('../utils/auth')
 const { User, List, Friend, UserFriend, Product } = require('../models');
 
 
-router.get('/', (req, res) => {
-    res.render('userboard')
-})
+router.get ('/', (req, res) => {
+    User.findAll({}).then(results => {
+        const user = results.map(post => post.get({ plain: true }));
 
+        res.render('userboard', {
+            user,
+            loggedIn: req.session.loggedIn
+        });
+    }).catch(err => {
+        confirm.log(err);
+        res.status(500).json(err);
+    });
+})
 
 
 module.exports = router;
